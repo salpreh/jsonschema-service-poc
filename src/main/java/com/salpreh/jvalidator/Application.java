0d7603796d93f7e1e9e4f85ddb89fe4a56d7f2ca
salpreh <salva.perez46@gmail.com>
2023-08-12 19:09:05 +0200

    Application bootstrap refactor

--- src/main/java/com/salpreh/jvalidator/Application.java
+++ src/main/java/com/salpreh/jvalidator/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class JvalidatorApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(JvalidatorApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
