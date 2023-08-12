56adcb7bfdbd3415228dc71d20ac9776f9870927
salpreh <salva.perez46@gmail.com>
2023-08-12 16:59:19 +0200

    Init repository

---
+++ src/main/java/com/salpreh/jvalidator/JvalidatorApplication.java
@@ -0,0 +1,13 @@
+package com.salpreh.jvalidator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JvalidatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JvalidatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/salpreh/jvalidator/JvalidatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.salpreh.jvalidator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JvalidatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
