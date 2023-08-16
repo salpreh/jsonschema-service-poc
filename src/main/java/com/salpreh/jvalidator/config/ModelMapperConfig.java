d9bbf05cfbf03b222ec8244bc5d62ace875336a1
salpreh <salva.perez46@gmail.com>
2023-08-16 08:39:11 +0200

    Custom contract mapping config

--- src/main/java/com/salpreh/jvalidator/config/ModelMapperConfig.java
+++ src/main/java/com/salpreh/jvalidator/config/ModelMapperConfig.java
@@ -1,6 +1,11 @@
 package com.salpreh.jvalidator.config;
 
+import com.salpreh.jvalidator.dtos.ContractDto;
+import com.salpreh.jvalidator.entities.ContractEntity;
+import com.salpreh.jvalidator.entities.ContractTypeEntity;
+import org.modelmapper.Converter;
 import org.modelmapper.ModelMapper;
+import org.modelmapper.TypeMap;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -9,6 +14,21 @@ public class ModelMapperConfig {
 
   @Bean
   public ModelMapper modelMapper() {
-    return new ModelMapper();
+    ModelMapper modelMapper = new ModelMapper();
+    contractMapping(modelMapper);
+
+    return modelMapper;
+  }
+
+  private void contractMapping(ModelMapper mapper) {
+    Converter<Long, ContractTypeEntity> idToEntity = ctx -> {
+      ContractTypeEntity entity = new ContractTypeEntity();
+      entity.setId(ctx.getSource());
+      return entity;
+    };
+    mapper.addConverter(idToEntity);
+
+    TypeMap<ContractEntity, ContractDto> entityToDto = mapper.createTypeMap(ContractEntity.class, ContractDto.class);
+    entityToDto.addMapping(e -> e.getContractType().getId(), ContractDto::setContractTypeId);
   }
 }
